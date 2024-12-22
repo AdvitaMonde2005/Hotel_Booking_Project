13a36b45ac5d4b97cf1fe155bf0b7dbc1e292e87
Advita Monde <advitamonde506@gmail.com>
2024-12-22 13:41:07 +0530

    Navbar Created

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adhruva Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <style>
+        *{
+            font-family: "Poppins", serif;
+        }
+        .h-font{
+            font-family: "Merienda", serif;
+        }
+    </style>
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-2 shadow-sm sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Adhruva Hotel</a>
+    <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+         <li class="nav-item">
+            <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link  me-2" href="#">Rooms</a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link  me-2" href="#">Facilites</a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link  me-2" href="#">Contact us</a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link  me-2" href="#">About us</a>
+         </li>
+     </ul>
+      <div class="d-flex" role="search">
+         <button class="btn btn-outline-success shadow-null" type="submit">Search</button>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
